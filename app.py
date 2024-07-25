a71c398f424f3566fd118dbca2a74d6e3077dc67
Jann Winter <jannwinter@icloud.com>
2024-07-25 11:13:48 +0200

    connect to port

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def upload_image():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
